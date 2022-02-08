75daf011533d2efcf8cbb301c6662516832cf18f
Abdallah <abdallah.m@padoa-group.com>
2022-02-09 00:19:56 +0100

    refacto(error) refacto error page

--- components/pepperError/pepperError.tsx
+++ components/pepperError/pepperError.tsx
@@ -3,15 +3,16 @@ import {
   View, Text, Button, StyleSheet 
 } from 'react-native';
 import PepperImage, { PepperImages } from '../pepperImage/pepperImage';
-import { space_unit, fontSizeBody } from '../../styles/common';
+import { space_unit, fontSizeRegular } from '../../styles/common';
 import * as SecureStore from 'expo-secure-store';
 import { UtilService } from '../../services/util';
 
 const PepperError = (): JSX.Element => (
   <View style={styles.container}>
     <PepperImage src={PepperImages.Sos} style={styles.image}></PepperImage>
-    <Text style={styles.description}></Text>
-    <Button title="Reload" onPress={async() => {
+    <Text style={styles.description}> The app is on fire!!</Text>
+    <Text style={{ ...styles.description, marginBottom: 4 * space_unit }}> we're calling the firemen </Text>
+    <Button title="Reload" onPress={ async() => {
       await SecureStore.deleteItemAsync('error');
       UtilService.reloadApp();
     }}/>
@@ -34,7 +35,6 @@ const styles = StyleSheet.create({
   description: {
     width: '80%',
     textAlign: 'center',
-    fontSize: fontSizeBody,
-    marginBottom: 2 * space_unit,
+    fontSize: fontSizeRegular,
   },
 });
